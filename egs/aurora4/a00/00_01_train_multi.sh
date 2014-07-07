6ca61b15d56b8c6e288e5b45f710f18f9e55418b
Troy Lee <troy.lee2008@gmail.com>
2014-07-07 16:09:12 +0800

    merge the training steps

--- egs/aurora4/a00/00_01_train_multi.sh
+++ egs/aurora4/a00/00_01_train_multi.sh
@@ -114,9 +114,56 @@ train_multi_spr_tri1b(){
 #train_multi_spr_tri1b
 
 ###############################################
+#Verifying old DNN setup, using the existing RBM
+# for self-check only
+
+prepare_dnn2a_pretrain(){
+  log_start "dnn2a [pretrain]"
+  dir=exp_multi/dnn2a_pretrain
+  srcdir=/media/research/dstc_aurora4
+  mkdir $dir
+  cp $srcdir/exps/multi_16k/b00_dnn/nnet/hid06c_transf $dir
+  ln -s $srcdir/exps/multi_16k/a00_hmm/tri1a $dir/tri1a
+  cp $srcdir/lib/fsts/wsj_5cnp/words.txt $dir/tri1a/graph_bcb05cnp/words.txt
+  log_end "dnn2a [pretrain]"
+}
+#prepare_dnn2a_pretrain 
+
+train_dnn2b(){
+  log_start "dnn2b [train]"
+  dir=exp_multi/dnn2b
+  dbn=exp_multi/dnn2a_pretrain/hid06c_transf
+  mkdir -p $dir/log
+  steps/train_nnet.sh --norm-vars true --dbn $dbn --hid-layers 0 --learn-rate 0.015 \
+    --labels ark:exp_multi/dnn2a_pretrain/tri1a/pdf_align/train_dev.pdf \
+    --alidir exp_multi/dnn2a_pretrain/tri1a/pdf_align \
+    feat/fbank/train_multi feat/fbank/dev_multi data/lang $dir || exit 1;
+  log_end "dnn2b [train]"
+}
+#train_dnn2b
+
+decode_dnn2b(){
+  log_start "dnn2b [decode]"
+  inv_acwt=17
+  acwt=`perl -e "print (1.0/$inv_acwt);"`;
+  for i in `seq -f "%02g" 1 14`; do
+    x=test${i}
+    steps/decode_nnet.sh --stage 1 --nj 8 --acwt $acwt --beam 15.0 --latbeam 9.0 --srcdir exp_multi/dnn2b \
+      --model exp_multi/dnn2a_pretrain/tri1a/pdf_align/final.mdl \
+      --class-frame-counts exp_multi/dnn2a_pretrain/tri1a/pdf_align/train.counts \
+      exp_multi/dnn2a_pretrain/tri1a/graph_bcb05cnp feat/fbank/${x} exp_multi/dnn2b/decode/decode_bg_${x} || exit 1;
+    exit 0;
+  done
+  local/average_wer.sh 'exp_multi/dnn2b/decode/decode_bg_test*' | tee exp_multi/dnn2b/decode/decode_bg_test.avgwer
+  log_end "dnn2b [decode]"
+
+}
+#decode_dnn2b
+
+###############################################
 #Now begin train DNN systems on multi data
 
-pretrain(){
+pretrain_dnn1a(){
   #RBM pretrain
   log_start "dnn1a [pretrain]"
   dir=exp_multi/dnn1a_pretrain
@@ -124,6 +171,6 @@ pretrain(){
   steps/pretrain_dnn.sh --nn-depth 7 --norm-vars true --splice 5 feat/fbank/train_multi $dir
   log_end "dnn1a [pretrain]"
 }
-pretrain
+pretrain_dnn1a
 
 
