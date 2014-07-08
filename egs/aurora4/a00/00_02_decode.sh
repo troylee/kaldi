45100cc77172c2a977b4bae9dc0dfaa94eb3d356
Troy Lee <troy.lee2008@gmail.com>
2014-07-08 11:23:05 +0800

    decode dnn

--- egs/aurora4/a00/00_02_decode.sh
+++ egs/aurora4/a00/00_02_decode.sh
@@ -41,7 +41,7 @@ decode_clean_tri1a(){
       if [ $sid -le 14 ]; then
         printf -v x 'test%02g' $sid
         echo ${nodes[$j]} $x
-        ( ssh ${nodes[$j]} "cd $cwd; steps/decode_deltas.sh --nj 5 --srcdir exp_clean/tri1a exp_clean/tri1a/graph_bg feat/mfcc/${x} exp_clean/tri1a/decode/decode_bg_${x}" ) &
+        ( ssh ${nodes[$j]} "cd $cwd; steps/decode_deltas.sh --nj 8 --srcdir exp_clean/tri1a exp_clean/tri1a/graph_bg feat/mfcc/${x} exp_clean/tri1a/decode/decode_bg_${x}" ) &
       fi
     done
     wait;
@@ -62,7 +62,7 @@ decode_clean_tri1b(){
       if [ $sid -le 14 ]; then
         printf -v x 'test%02g' $sid
         echo ${nodes[$j]} $x
-        ( ssh ${nodes[$j]} "cd $cwd; steps/decode_deltas.sh --nj 5 --srcdir exp_clean/tri1b exp_clean/tri1b/graph_bg feat/mfcc/${x} exp_clean/tri1b/decode/decode_bg_${x}" ) &
+        ( ssh ${nodes[$j]} "cd $cwd; steps/decode_deltas.sh --nj 8 --srcdir exp_clean/tri1b exp_clean/tri1b/graph_bg feat/mfcc/${x} exp_clean/tri1b/decode/decode_bg_${x}" ) &
       fi
     done
     wait;
@@ -83,7 +83,7 @@ decode_clean_tri1b_vtsmodel(){
       if [ $sid -le 14 ]; then
         printf -v x 'test%02g' $sid
         echo ${nodes[$j]} $x
-        ( ssh ${nodes[$j]} "cd $cwd; steps/decode_vts_model.sh --nj 5 --srcdir exp_clean/tri1b exp_clean/tri1b/graph_bg feat/mfcc/${x} exp_clean/tri1b/decode_vts_model/decode_bg_${x}" ) &
+        ( ssh ${nodes[$j]} "cd $cwd; steps/decode_vts_model.sh --nj 8 --srcdir exp_clean/tri1b exp_clean/tri1b/graph_bg feat/mfcc/${x} exp_clean/tri1b/decode_vts_model/decode_bg_${x}" ) &
       fi
     done
     wait;
@@ -127,7 +127,7 @@ decode_multi_tri1b(){
       if [ $sid -le 14 ]; then
         printf -v x 'test%02g' $sid
         echo ${nodes[$j]} $x
-        ( ssh ${nodes[$j]} "cd $cwd; steps/decode_deltas.sh --nj 5 --srcdir exp_multi/tri1b exp_multi/tri1b/graph_bg feat/mfcc/${x} exp_multi/tri1b/decode/decode_bg_${x}" ) &
+        ( ssh ${nodes[$j]} "cd $cwd; steps/decode_deltas.sh --nj 8 --srcdir exp_multi/tri1b exp_multi/tri1b/graph_bg feat/mfcc/${x} exp_multi/tri1b/decode/decode_bg_${x}" ) &
       fi
     done
     wait;
@@ -137,7 +137,7 @@ decode_multi_tri1b(){
   local/average_wer.sh 'exp_multi/tri1b/decode/decode_bg_test*' | tee exp_multi/tri1b/decode/decode_bg_test.avgwer
   log_end "tri1b [decode]"
 }
-decode_multi_tri1b
+#decode_multi_tri1b
 
 decode_multi_tri1b_vtsmodel(){
   log_start "tri1b [vtsmodel decode]"
@@ -148,7 +148,7 @@ decode_multi_tri1b_vtsmodel(){
       if [ $sid -le 14 ]; then
         printf -v x 'test%02g' $sid
         echo ${nodes[$j]} $x
-        ( ssh ${nodes[$j]} "cd $cwd; steps/decode_vts_model.sh --nj 5 --srcdir exp_multi/tri1b exp_multi/tri1b/graph_bg feat/mfcc/${x} exp_multi/tri1b/decode_vts_model/decode_bg_${x}" ) &
+        ( ssh ${nodes[$j]} "cd $cwd; steps/decode_vts_model.sh --nj 8 --srcdir exp_multi/tri1b exp_multi/tri1b/graph_bg feat/mfcc/${x} exp_multi/tri1b/decode_vts_model/decode_bg_${x}" ) &
       fi
     done
     wait;
@@ -158,9 +158,9 @@ decode_multi_tri1b_vtsmodel(){
   local/average_wer.sh 'exp_multi/tri1b/decode_vts_model/decode_bg_test*' | tee exp_multi/tri1b/decode_vts_model/decode_bg_test.avgwer
   log_end "tri1b [vtsmodel decode]"
 }
-decode_multi_tri1b_vtsmodel
+#decode_multi_tri1b_vtsmodel
 
-## dnn2b is the old training setup
+## dnn2b is the old RBMs, old tri1a and new features and fine-tuning
 decode_multi_dnn2b(){
   log_start "dnn2b [decode]"
   inv_acwt=17
@@ -183,3 +183,28 @@ decode_multi_dnn2b(){
   log_end "dnn2b [decode]"
 }
 #decode_multi_dnn2b
+
+## dnn2c is using the old RBMs, new tri1a, new features and new fine-tuning
+decode_multi_dnn2c(){
+  log_start "dnn2c [decode]"
+  inv_acwt=17
+  acwt=`perl -e "print (1.0/$inv_acwt);"`;
+  i=1
+  while [ $i -le 14 ]; do
+    for j in `seq 0 $numNodes`; do
+      sid=$((i+j))
+      if [ $sid -le 14 ]; then
+        printf -v x 'test%02g' $sid
+        echo ${nodes[$j]} $x
+        ( ssh ${nodes[$j]} "cd $cwd; steps/decode_nnet.sh --nj 8 --acwt $acwt --beam 15.0 --latbeam 9.0 --srcdir exp_multi/dnn2c exp_multi/dnn2c/graph_bg feat/fbank/${x} exp_multi/dnn2c/decode/decode_bg_${x}" ) &
+      fi
+    done
+    wait;
+    i=$((sid+1))
+  done
+  # write out the average WER results
+  local/average_wer.sh 'exp_multi/dnn2c/decode/decode_bg_test*' | tee exp_multi/dnn2c/decode/decode_bg_test.avgwer
+  log_end "dnn2c [decode]"
+}
+#decode_multi_dnn2c
+
